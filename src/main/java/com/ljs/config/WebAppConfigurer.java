4422e2e81682d17a908936e37ba4d1719bde7c27
ljsyy <56251257+ljsyy@users.noreply.github.com>
2020-03-18 23:02:15 +0800

    WebApp配置类

---
+++ src/main/java/com/ljs/config/WebAppConfigurer.java
@@ -0,0 +1,17 @@
+package com.ljs.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebAppConfigurer implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowCredentials(true)
+                .allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE","OPTIONS")
+                .maxAge(3600);
+    }
+}
